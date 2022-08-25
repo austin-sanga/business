20a7cd6acda92ac49189a712cbfcae2c47f433ac
austin-sanga <austinsanga2000@gmail.com>
2022-08-25 10:43:47 +0300

    changes

--- resources/views/investments.blade.php
+++ resources/views/investments.blade.php
@@ -2,5 +2,27 @@
 
 @section('content')
 
+<h5 class="text-center" style="color:#0B9050 ">Investments</h5>
+
+<div class="{{-- col-md-6  --}}{{-- col-xl-3 --}} mb-4 mx-5">
+    <a href="ministatement" class="text-decoration-none">{{-- link to the mini-statement --}}
+    <div class="card shadow border-start-primary py-2" >
+        <div class="card-body">
+            <div class="row align-items-center no-gutters">
+                <div class="col-auto">
+                    <i class="bi bi-journals" style="font-size: 40px; color:black"></i>
+                </div>
+                <div class="col me-2">
+                    <div class=" fw-bold text-black text-xs mb-1 " {{-- style="color: #0B9050" --}}><span>Investment accounts</span></div>
+                 </div>
+                 <div class="col-auto">
+                    <i class="bi bi-arrow-right-short" style="font-size: 40px; color:#0B9050"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+    </a>
+</div>
+
 
 @stop
--- resources/views/ministatement.blade.php
+++ resources/views/ministatement.blade.php
@@ -2,5 +2,19 @@
 
 @section('content')
 
+<h5 class="text-center" style="color:#0B9050 ">Mini Statement</h5>
+
+<div class="container bg-white">
+    {{-- after this is where the loop should be appearing --}}
+    <div class="px-3">
+        <h6>{{-- Date of transaction--}}Tuesday, 19 july 2022</h6>
+        <div class="row ">
+        <div class="col"><span>{{-- the prior word should be depending on the activity --}}Invested  mr kuku</span></div>
+        <div class="col  justify-content-end text-end"><span>{{-- number --}}25,000,000TZS</span></div>
+        </div>
+        <hr>
+    </div>
+
+</div>
 
 @stop
