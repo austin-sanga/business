571099a4dfbdc1673b5c880a506c25477a1b53db
austin-sanga <austinsanga2000@gmail.com>
2022-08-31 00:32:45 +0300

    ongoing investment

--- resources/views/ongoinginvestment.blade.php
+++ resources/views/ongoinginvestment.blade.php
@@ -44,14 +44,14 @@
     <div class="px-3">
         <div class="row ">
         <div class="col"><span>Contract copy</span></div>
-        <div class="col  justify-content-end text-end"><button class="rounded px-3" style="background-color: #0B9050">download</button></div>{{-- contract download link --}}
+        <div class="col  justify-content-end text-end"><button class="rounded px-4" style="background-color: #0B9050; color:white">download</button></div>{{-- contract download link --}}
         </div>
         <hr>
     </div>
     <div class="m-3">
-        <div><h4 class="text-muted ">Project manager notice</h4></div>
+        <div><h5 class="text-muted ">Project manager notice</h5></div>
         <div class="rounded p-2" style="background-color: #F5F5F5 ">
-            <h4>{{-- title --}} Lorem, ipsum dolor.</h4>
+            <h5>{{-- title --}} Lorem, ipsum dolor.</h5>
             <p>{{-- paragraph of content --}} Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel ducimus quos, vero minima sint culpa dolores eligendi omnis ut iste magnam reiciendis dicta inventore quam aut non. Odit distinctio, debitis cumque alias nulla, sunt sint sed nihil, cum a molestias magni architecto consequatur autem provident quaerat reiciendis rerum. Facere odit magnam est doloremque minima eaque minus dicta deserunt debitis optio?</p>
         </div>
     </div>
