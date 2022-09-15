857ee1f0018f8140cf5497d73721497d9fcc69fa
austin-sanga <austinsanga2000@gmail.com>
2022-09-15 14:09:10 +0300

    error

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -7,20 +7,20 @@
       <div class="collapse navbar-collapse justify-content-around" id="navbarNav" >
         <ul class="navbar-nav">
           <li class="nav-item me-5">
-            <a class="nav-link active" aria-current="page" href="dashboard">Home</a>
+            <a class="nav-link active" aria-current="page" href="/dashboard">Home</a>
           </li>
           <li class="nav-item me-5">
-            <a class="nav-link" href="investments">Investments</a>
+            <a class="nav-link" href="/investments">Investments</a>
           </li>
           <li class="nav-item me-5">
-            <a class="nav-link" href="loans">Loans</a>
+            <a class="nav-link" href="/loans">Loans</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="settings" {{-- tabindex="-1" aria-disabled="true" --}}>settings</a>
+            <a class="nav-link" href="/settings" {{-- tabindex="-1" aria-disabled="true" --}}>settings</a>
           </li>
         </ul>
       </div>
     </div>
   </nav>
 
- 
+
